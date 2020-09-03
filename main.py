021664ae98c27212d501774e5307bb7f0d1f4754
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-03 21:00:28 +0000

    fixing spelling

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ if unit == "F" or unit == "f":
   print(f"{temp}° in Fahrenheit is equivalent to {temp2}° Celsius.")
 
 elif unit == "C" or unit == "c":
-  print(f"{temp}° in Celsiud is equivalent to {temp1}° Fahrenheit.")
+  print(f"{temp}° in Celsius is equivalent to {temp1}° Fahrenheit.")
 else:
   print(f"Invalid unit({unit}).")

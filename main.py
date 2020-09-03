d05ddf6a7d013d146e9f5dec10997f880887b037
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-03 20:59:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Author: Shakeb Siddiqui sms8508@psu.edu
+#Collaborator: Dominic Savaglio djs7129@psu.edu
+#Collaborator: Sneha Prem Chandran
+#Collaborator: Samantha Zolin saz5193@psu.edu
+inp = input("Enter temperature: ")
+
+unit = input("Enter unit in F/f or C/c: ")
+temp = float(inp)
+temp1 = temp * 9/5 +32
+temp2 = (temp -32) / 1.8
+
+if unit == "F" or unit == "f":
+  print(f"{temp}° in Fahrenheit is equivalent to {temp2}° Celsius.")
+
+elif unit == "C" or unit == "c":
+  print(f"{temp}° in Celsiud is equivalent to {temp1}° Fahrenheit.")
+else:
+  print(f"Invalid unit({unit}).")
